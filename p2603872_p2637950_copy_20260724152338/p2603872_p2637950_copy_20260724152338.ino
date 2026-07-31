9e752de0da1d61005c2cb8489c1164a11b83d672
vivekanandanvarshita-debug <vivekanandanvarshita@gmail.com>
2026-07-31 15:45:42 +0800

    Update p2603872_p2637950_copy_20260724152338.ino
    number

--- p2603872_p2637950_copy_20260724152338/p2603872_p2637950_copy_20260724152338.ino
+++ p2603872_p2637950_copy_20260724152338/p2603872_p2637950_copy_20260724152338.ino
@@ -5,14 +5,14 @@
 
 #define LED_RED 4
 #define LED_GREEN 5
-#define LED_YELLOW 5 hello
+#define LED_YELLOW 5
 #define BUTTON_K1 8
 #define  BUTTON_K2 9
-#define   BUZZER 3 hi 
+#define   BUZZER 3
 #define CLK 10 //CLK of the TM1637 IC connected to D10 of OPEN-SMART UNO R3
 #define DIO 11 
 // by varshita
-
+34
 void setup() {
   // put your setup code here, to run once:
 
