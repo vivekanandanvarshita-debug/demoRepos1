8892ed5805baad5151180fd6d9add8fb1ba3fa37
vivekanandanvarshita-debug <vivekanandanvarshita@gmail.com>
2026-07-31 15:19:15 +0800

    Update p2603872_p2637950_copy_20260724152338.ino

--- p2603872_p2637950_copy_20260724152338/p2603872_p2637950_copy_20260724152338.ino
+++ p2603872_p2637950_copy_20260724152338/p2603872_p2637950_copy_20260724152338.ino
@@ -5,9 +5,9 @@
 
 #define LED_RED 4
 #define LED_GREEN 5
-#define LED_YELLOW 5
+#define LED_YELLOW 5 hello i am varshita
 #define BUTTON_K1 8
-#define  BUTTON_K2 9 hello
+#define  BUTTON_K2 9
 #define   BUZZER 3
 #define CLK 10 //CLK of the TM1637 IC connected to D10 of OPEN-SMART UNO R3
 #define DIO 11 
